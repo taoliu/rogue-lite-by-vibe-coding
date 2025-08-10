cbe2195e3eb6ab86e26a6742db87858873b8241a
Tao Liu (τν) <vladimir.liu@gmail.com>
2025-08-10 10:13:44 -0400

    Load sprite textures from resource folder

--- sprites.js
+++ sprites.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 
 const SPRITE_SIZE = 96;
+const loader = new THREE.TextureLoader();
 
 function makeSprite(color, emoji) {
   const canvas = document.createElement('canvas');
@@ -22,21 +23,37 @@ function makeSprite(color, emoji) {
   return tex;
 }
 
-const cache = {
-  player_warrior: makeSprite('#aa0000'),
-  player_mage: makeSprite('#0000aa'),
-  player_hunter: makeSprite('#008800'),
-  Goblin: makeSprite('#00aa00'),
-  'Skeleton Archer': makeSprite('#ffffff'),
-  Orc: makeSprite('#225500'),
-  Zombie: makeSprite('#99cc00'),
-  Mimic: makeSprite('#8b5e34'),
-  Ogre: makeSprite('#553300'),
-  'Young Dragon': makeSprite('#ff0000'),
-  'Crystal Guardian': makeSprite('#00ffff'),
-  merchant: makeSprite('#996633'),
-  default: makeSprite('#ff00ff')
+const SPRITE_INFO = {
+  player_warrior: '#aa0000',
+  player_mage: '#0000aa',
+  player_hunter: '#008800',
+  Goblin: '#00aa00',
+  'Skeleton Archer': '#ffffff',
+  Orc: '#225500',
+  Zombie: '#99cc00',
+  Mimic: '#8b5e34',
+  Ogre: '#553300',
+  'Young Dragon': '#ff0000',
+  'Crystal Guardian': '#00ffff',
+  merchant: '#996633',
+  default: '#ff00ff'
 };
+
+const cache = {};
+
+for (const [name, color] of Object.entries(SPRITE_INFO)) {
+  const tex = loader.load(
+    `resource/${encodeURIComponent(name)}.jpeg`,
+    undefined,
+    undefined,
+    () => {
+      cache[name] = makeSprite(color);
+    }
+  );
+  tex.magFilter = THREE.NearestFilter;
+  tex.minFilter = THREE.NearestFilter;
+  cache[name] = tex;
+}
 
 export function getSprite(name) {
   return cache[name] || cache.default;
